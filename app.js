8c9b30a3297c4720fd479c6a934b7296fa5eae1a
Shmuel atar <shmuelatar@gmail.com>
2025-12-19 10:24:25 +0200

    fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 8080
 
 
-app.get("/", (req, res) => { res.send("bye bye gal i am going home! shabbat shalom! :)") })
+app.get("/", (req, res) => { res.send("bye  bye gal i am going home! shabbat shalom! :)") })
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
