966221386e82bdaf76dac04b311290f3206d2e32
Shmuel atar <shmuelatar@gmail.com>
2025-12-19 11:03:03 +0200

    new html page of CV

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express()
 const port = 8080
 
 
-app.get("/", (req, res) => { res.send("bye bye gal i am going home! shabbat shalom! :)") })
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "page.html"));
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
